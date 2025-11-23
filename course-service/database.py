c0a270acd17f9a19fe5bd48babaee840bd86a1a3
G00403720 <G00403720@atu.ie>
2025-11-23 23:03:32 +0000

    Course Service

---
+++ course-service/database.py
@@ -0,0 +1,35 @@
+import os, time 
+from dotenv import load_dotenv 
+from sqlalchemy import create_engine 
+from sqlalchemy.orm import sessionmaker 
+from sqlalchemy.exc import OperationalError 
+
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./app.db")
+SQL_ECHO = os.getenv("SQL_ECHO", "false").lower() == "true" 
+RETRIES = int(os.getenv("DB_RETRIES", "10")) 
+DELAY = float(os.getenv("DB_RETRY_DELAY", "1.5")) 
+
+connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else{}
+
+engine = create_engine(DATABASE_URL, echo = True, connect_args=connect_args)
+SessionLocal = sessionmaker(bind=engine, expire_on_commit=False)
+
+for _ in range(RETRIES): 
+    try: 
+        engine = create_engine(DATABASE_URL, pool_pre_ping=True, echo=SQL_ECHO, 
+connect_args=connect_args) 
+        with engine.connect():  
+            pass 
+        break 
+    except OperationalError: 
+        time.sleep(DELAY) 
+ 
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False, 
+expire_on_commit=False) 
+ 
+def get_db(): 
+    db = SessionLocal() 
+    try: 
+        yield db 
+    finally: 
+        db.close() 
---
+++ course-service/models.py
@@ -0,0 +1,15 @@
+
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import declarative_base,  Mapped, mapped_column
+
+#Creates base class for table
+Base = declarative_base()
+
+#Course database
+class Coursedb(Base): 
+    __tablename__ = "courses" 
+
+    id: Mapped[int] = mapped_column(primary_key=True) 
+    code: Mapped[str] = mapped_column(unique=True)           
+    name: Mapped[str] = mapped_column(String, nullable=False)          
+    owner_id: Mapped[int] = mapped_column(Integer, nullable=False)
